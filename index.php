90d4947a9e0687392b52633060dbd7777afeb025
tikaram banjade <tika.banjade@olivegroup.io>
2026-09-04 22:51:32 +0545

    add text in index

--- index.php
+++ index.php
@@ -5,5 +5,8 @@
 
     <body>
           <h1>Git tutorials</h1>
+          <h2>Git rebase tutorials</h2>
+          <p>Git rebase is a powerful tool that allows you to modify your commit history </p>
+          <p>It is often used to clean up your commit history before merging changes into a
     </body>
 </html>
